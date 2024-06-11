798f036c372273638608ea8831014631773311b9
AshiniNawodya7 <ashininavodya76@gmail.com>
2024-06-11 10:49:02 +0530

    first commit

---
+++ Uwu.java
@@ -0,0 +1,30 @@
+public class Uwu {
+    public static void main(String[] args) {
+        boolean test1=true;
+        boolean test2=true;
+
+        if (test1){
+            System.out.println("true block executed");
+
+        }
+
+        else if(test2){
+            System.out.println("this is the second line");
+        }
+        else{
+            System.out.println("else block executed");
+
+
+        }
+
+        
+    }
+
+
+
+
+
+}
+
+
+
